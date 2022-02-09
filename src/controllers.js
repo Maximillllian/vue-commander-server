eb8e9b449148b7dd2cb89db6971877d47293568d
Max <voronin.maksim@mail.com>
2022-02-09 20:00:44 +0500

    add features: copy and move files

--- src/controllers.js
+++ src/controllers.js
@@ -1,5 +1,6 @@
 import drivelist from "drivelist";
 import fs from "fs";
+import fse from 'fs-extra'
 import path from "path";
 import open from 'open'
 import { getFileType, isRoot } from './utils.js'
@@ -87,5 +88,38 @@ export function deleteFiles(req, res) {
         })
     } catch (err) {
         console.log(err)
+    }
+}
+
+export function copyFiles(req, res) {
+    try {
+        const { files } = req.body;
+        files.forEach(file => {
+            fse.copySync(file.sourcePath, file.destinationPath);
+        })
+        return res.json({
+            message: 'Files succesfully copied'
+        })
+    } catch (err) {
+        res.status(404).json({
+            message: 'Ошибка при копировании файлов, проверьте путь'
+        })
+    }
+}
+
+export function moveFiles(req, res) {
+    try {
+        const { files } = req.body;
+        files.forEach(file => {
+            fs.renameSync(file.sourcePath, file.destinationPath);
+        })
+        return res.json({
+            message: 'Files succesfully moved'
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(404).json({
+            message: 'Ошибка при перемещении файлов, проверьте путь'
+        })
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -8,5 +8,7 @@ router.post('/open-folder', controllers.openFolder)
 router.post('/open-file-native', controllers.openFileNative)
 router.post('/back', controllers.getParentFolder)
 router.post('/delete', controllers.deleteFiles)
+router.post('/copy', controllers.copyFiles)
+router.post('/move', controllers.moveFiles)
 
 export default router;
